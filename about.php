99417311f7f01a9387d17eb9e8e9245e21d084e0
karan singh <karan.singh@ebslon.com>
2021-10-30 15:24:09 +0530

    Started working on about page and added page-banner

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php include("header.php");?>
+
+<main>
+    <div class="page-banner heading">
+        <div class="container">
+            <div class="page-absolute">
+                <h2>About Us</h2>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li>About Us</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php include("footer.php");?>
