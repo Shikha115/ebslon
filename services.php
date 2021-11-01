c018f12539f7a430684aa01c9cdb33031aa7a926
karan singh <karan.singh@ebslon.com>
2021-11-01 16:15:44 +0530

    added service page offer section, testimonials and clients

--- services.php
+++ services.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div class="page-absolute">
                 <p>Welcome to our company</p>
-                <h2>What we offer</h2>
+                <h2>What we offer?</h2>
                 <ul>
                     <li><a href="index.php">Home</a></li>
                     <li>Our services</li>
@@ -16,7 +16,7 @@
 
     <section class="service-page mb-80">
         <div class="container">
-            <div class="row">
+            <div class="row align-items-center">
                 <div class="col-12 col-md-6 col-adjust pl-0">
                     <div class="service-page-left">
                         <img src="src/images/service-page.PNG" alt="services">
@@ -31,38 +31,38 @@
                         <div class="tab-content" id="pills-tabContent">
                             <div class="tab-pane fade show active" id="Digital-Marketing" role="tabpanel">
                                 <div class="service-page-content">
-                                    <p>Our digital marketing services are as unique as your business. Experts at Ebslon tend to understand the length and breadth of your business operation, business objectives and explicit customer base before prescribing a digital strategy.</p>
+                                    <p class='mb-4'>Our digital marketing services are as unique as your business. Experts at Ebslon tend to understand the length and breadth of your business operation, business objectives and explicit customer base before prescribing a digital strategy.</p>
                                     <a href="single-service.php" class="btn btn-2 rounded-0">Read More</a>
                                 </div>
                             </div>
                             <div class="tab-pane fade" id="Web-Development" role="tabpanel">
                                 <div class="service-page-content">
-                                    <p>Ebslon is one of the leading website development companies in India. In over a decade of its presence, the company has established itself as a reliable website design and website development company.</p>
+                                    <p class='mb-4'>Ebslon is one of the leading website development companies in India. In over a decade of its presence, the company has established itself as a reliable website design and website development company.</p>
                                     <a href="single-service.php" class="btn btn-2 rounded-0">Read More</a>
                                 </div>
                             </div>
                             <div class="tab-pane fade" id="Website-Designing" role="tabpanel">
                                 <div class="service-page-content">
-                                    <p>At Ebslon, we recognize, well in advance, the trends, updates and paradigm shifts happening in the web-designing world. We realize the significance of website design and the way it is unique to a particular company.</p>
+                                    <p class='mb-4'>At Ebslon, we recognize, well in advance, the trends, updates and paradigm shifts happening in the web-designing world. We realize the significance of website design and the way it is unique to a particular company.</p>
                                     <a href="single-service.php" class="btn btn-2 rounded-0">Read More</a>
                                 </div>
                             </div>
                             <div class="tab-pane fade" id="Content-Writing" role="tabpanel">
                                 <div class="service-page-content">
-                                    <p>Our content writers blend the best elements of SEO, SEM, and professional copywriting to create page content and product descriptions that stand out from the crowd. Contact Ebslon InfoTech today for your content needs related to website, blogs
+                                    <p class='mb-4'>Our content writers blend the best elements of SEO, SEM, and professional copywriting to create page content and product descriptions that stand out from the crowd. Contact Ebslon InfoTech today for your content needs related to website, blogs
                                         or articles.</p>
                                     <a href="single-service.php" class="btn btn-2 rounded-0">Read More</a>
                                 </div>
                             </div>
                             <div class="tab-pane fade" id="Mobile-App-Development" role="tabpanel">
                                 <div class="service-page-content">
-                                    <p>Ebslon, one of the World’s fastest growing Mobile App Development Company has over a decade of experience in building world-class B2B and B2C web-applications.</p>
+                                    <p class='mb-4'>Ebslon, one of the World’s fastest growing Mobile App Development Company has over a decade of experience in building world-class B2B and B2C web-applications.</p>
                                     <a href="single-service.php" class="btn btn-2 rounded-0">Read More</a>
                                 </div>
                             </div>
                             <div class="tab-pane fade" id="Website-Hosting" role="tabpanel">
                                 <div class="service-page-content">
-                                    <p>Ebslon provides a complete website hosting solution with full functionality to business houses, government agencies, NGOs, and individual clients. Our clients get exceptional 24/7 expert support services at the quickest possible time.</p>
+                                    <p class='mb-4'>Ebslon provides a complete website hosting solution with full functionality to business houses, government agencies, NGOs, and individual clients. Our clients get exceptional 24/7 expert support services at the quickest possible time.</p>
                                     <a href="single-service.php" class="btn btn-2 rounded-0">Read More</a>
                                 </div>
                             </div>
@@ -138,6 +138,129 @@
                     </a>
                 </li>
             </ul>
+        </div>
+    </section>
+
+    <section class="offer mb-80 position-relative">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-6 col-adjust pl-0">
+                    <div class="title-section">
+                        <h6>WHAT ARE YOU WAITING FOR?</h6>
+                        <h4 class='text-left'>Our Offer <span>For You</span></h4>
+                    </div>
+                    <p>Each completed project makes us even more hungry, hungry for more designs, more code and at least some more twinkies. As a result we deliver a better web experience.</p>
+                    <div class="offer-range">
+                        <h5>Web Designing - 97%</h5>
+                        <div class="offer-width">
+                            <div class="offer-fill" style='width:97%;background-color:#fbaf17;'></div>
+                        </div>
+                    </div>
+                    <div class="offer-range">
+                        <h5>GRAPHIC DESIGNING - 80%</h5>
+                        <div class="offer-width">
+                            <div class="offer-fill" style='width:80%;background-color:#00bea3;'></div>
+                        </div>
+                    </div>
+                    <div class="offer-range">
+                        <h5>WEB DEVELOPMENT - 92%</h5>
+                        <div class="offer-width">
+                            <div class="offer-fill" style='width:92%;background-color:#f1894c;'></div>
+                        </div>
+                    </div>
+                    <div class="offer-range">
+                        <h5>APP DEVELOPMENT - 92%</h5>
+                        <div class="offer-width">
+                            <div class="offer-fill" style='width:92%;background-color:#a0ce4e;'></div>
+                        </div>
+                    </div>
+                    <div class="offer-range">
+                        <h5>DIGITAL MARKETING - 88%</h5>
+                        <div class="offer-width">
+                            <div class="offer-fill" style='width:88%;background-color:blue;'></div>
+                        </div>
+                    </div>
+                    <div class="offer-range">
+                        <h5>CUSTOMER SUPPORT - 100%</h5>
+                        <div class="offer-width">
+                            <div class="offer-fill" style='width:100%;background-color:skyblue;'></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-adjust pr-0">
+                    <img src="src/images/offer.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="testimonial-sec mb-80">
+       <div class="test-bg"></div>
+       <div class="container">
+           <div class="title-section">
+               <span>Testimonials</span>
+               <h4>What are Clients say about us?</h4>
+           </div>
+           <div class="owl-carousel owl-theme test-form-slider">
+               <div class="item">
+                   <div class="testimonial-inner">
+                       <img src="src/images/t1.jpg" alt="">
+                       <p><i class="fas fa-quote-left"></i>Thanks for a great job, I am very happy with the result. During the work process, they were very flexible and willing to try several things according to my suggestions. I’m sure I’ll hire them again!</p>
+                       <h4 class="heading">Aditya</h4>
+                       <span>US</span>
+                   </div>
+               </div>
+               <div class="item">
+                    <div class="testimonial-inner">
+                        <img src="src/images/t2.jpg" alt="">
+                        <p><i class="fas fa-quote-left"></i>Excellent company Ebslon! I will definitely work with them again. Good communication, good work, on time!</p>
+                        <h4 class="heading">Aman</h4>
+                        <span>INDIA</span>
+                    </div>
+               </div>
+               <div class="item">
+                    <div class="testimonial-inner">
+                        <img src="src/images/t3.jpg" alt="">
+                        <p><i class="fas fa-quote-left"></i>Professional, transparent and honest guys … you’ll love working with Ebslon.</p>
+                        <h4 class="heading">Madhu</h4>
+                        <span>India</span>
+                    </div>
+               </div>
+           </div>
+       </div>
+   </section>
+
+    <div class="container">
+        <hr class='m-0'>
+    </div>
+
+    <section class="client-sec">
+        <div class="container">
+            <div class="row">
+                <div class="owl-carousel owl-theme clients">
+                    <div class="item">
+                        <img src="src/images/clients/1.png" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="src/images/clients/2.png" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="src/images/clients/3.png" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="src/images/clients/4.png" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="src/images/clients/5.png" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="src/images/clients/6.png" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="src/images/clients/7.png" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
