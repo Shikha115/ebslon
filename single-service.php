a0c808ea2071ca6fcf96f795b713408705454aa7
karan singh <karan.singh@ebslon.com>
2021-11-01 17:29:05 +0530

    adding sections in single-service-page

---
+++ single-service.php
@@ -0,0 +1,62 @@
+<?php include("header.php");?>
+
+<main>
+    <section class="page-banner heading mb-80 single-service-banner">
+        <div class="container">
+            <div class="page-absolute">
+                <p>Welcome to our company</p>
+                <h2>Get to know us,<br>get to work with us</h2>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li>Web Designing</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="service-box1 mb-80">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-6 col-adjust pl-0">
+                    <div class="service-box1-content">
+                        <div class="title-section">
+                            <h4 class='text-left'>Web Development Company In India...</h4>
+                        </div>
+                        <p>A website represents your company; it’s essence and core values. It is one of the most powerful tools to realize your goals. It is the first impression on your clients and a communication gateway for them. A website is an impactful way to enhance the brand identity of your company, bring increased sale and expand your business.</p>
+                        <p>At Ebslon, we recognize, well in advance, the trends, updates and paradigm shifts happening in the web-designing world. We realize the significance of website design and the way it is unique to a particular company. Companies now need a relevant design that showcases their companies in a presentable and evocative way. They need a well navigable, customized web-design that generates an interactive user experience and the one that is digitally compatible with multiple devices. Our bouquet of website design services includes but not limited to websites, apps, social media identities and other digital decks.</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-adjust pr-0">
+                    <img src="src/images/8.jpg" alt="web-design">
+                </div>
+            </div>
+            <p>Our expert team across various segments follow a stylized process to create appropriate, responsive web-designs with strategic inputs, insights, and infusion of ideas. They analyze your business portfolio; understand the business’ requirements, the market, and the end consumer before creating a web-design.</p>
+            <p>We strive for offering you an “engineered” web-design, unique to your company that would surely generate bigger brand engagement, higher lead conversions & bring measurable overall results.Our websites are Enticing, Engaging and Engineered for your business’ only.</p>
+        </div>
+    </section>
+
+    <section class="service-box1 service-box2 mb-80 position-relative">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-6 col-adjust pl-0">
+                    <img src="src/images/4.jpg" alt="web-design" class='rounded-0 border-0 p-0'>
+                </div>
+                <div class="col-12 col-md-6 col-adjust pr-0">
+                    <div class="service-box1-content">
+                        <div class="title-section">
+                            <h4 class='text-left'>UI+UX Designs</h4>
+                        </div>
+                        <p>We deliver User Interface designs for websites that enable a user to maximize usability and the user experience. Our user-centered designs make the user’s interaction pretty simple, efficient and effective. Our UI+UX Designs ensures an easy navigation & interaction that enables a user to have uninterrupted access to all parts of the website and goes through all the data and information that our clients wish to share.</p>
+                        <div class="title-section">
+                            <h4 class='text-left'>Responsive Designs</h4>
+                        </div>
+                        <p class='m-0'>Different companies have unique approaches to reach out to their clients. They make every effort to tap on their user’s behavior and environment which fluctuate as per the on screen size, platform, and orientation. Thus the requirements of different digital platforms vary. Realising this, we provide responsive web design that is compatible with all devices and exhibit enchanting appearance across all platforms. We design for ALL USERS without compromising the essence of your website content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<?php include("footer.php");?>
