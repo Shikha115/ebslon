ccb3e44f061571d57afbf7b3708887298657a095
karan singh <karan.singh@ebslon.com>
2021-11-02 16:35:33 +0530

    working on contact page

---
+++ contact.php
@@ -0,0 +1,84 @@
+<?php include("header.php");?>
+
+<main>
+    <section class="page-banner heading mb-80">
+        <div class="container">
+            <div class="page-absolute">
+                <p>Welcome to our company</p>
+                <h2>Where to find us?</h2>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="contact-page-sec mb-80">
+        <div class="container">
+            <div class="row align-items-center mb-5">
+                <div class="col-12 col-md-6">
+                <div class="contact-page-right">
+                    <div class="title-section">
+                        <h4 class="text-left mt-0 mb-3">ebslon infotech</h4>
+                    </div>
+                    <p class="text-muted mb-4">Even if there is something you have always wanted to experience and can’t find it on Ebslon, let us know and we promise we’ll do our best to find it for you and send you there.</p>
+                    <a href="tel:+91 9999 063652" class='btn btn-2 rounded-0'><i class="fas fa-phone-alt"></i>+91 9999 063652</a>
+                </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="contact-page-left">
+                        <img src="src/images/contact.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row contact-details">
+                <div class="col-12 col-md-3">
+                    <div class="icon-box">
+                        <h6 class="fw-300 heading">
+                            <img src="src/images/map.png" class="y-color-ic">Our address
+                        </h6>
+                        <p class='m-0'>506-507,ITL Northex Tower, A09<br> Netaji Subhash Place,<br> Pitampura, Delhi-110034</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3">
+                    <div class="icon-box">
+                        <h6 class="fw-300 heading">
+                            <img src="src/images/smartphone.png" class="y-color-ic">Phone 24/7
+                        </h6>
+                        <p class='m-0'><a href="tel:+91 9999 063652">+91 9999 063652</a></p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3">
+                    <div class="icon-box">
+                        <h6 class="fw-300 heading">
+                            <img src="src/images/clock.png" class="y-color-ic">Operating Hours
+                        </h6>
+                        <p>Mon-Fri</p>
+                        <p class='m-0'>10.30 am - 7.30 pm</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3">
+                    <div class="icon-box">
+                        <h6 class="fw-300 heading">
+                            <img src="src/images/email.png" class="y-color-ic">Email address
+                        </h6>
+                        <p class='m-0'><a href="mailto:info@ebslon.com">info@ebslon.com</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="map">
+        <div class="container-fluid p-0">
+            <div class="row">
+                <iframe id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13999.670519619358!2d77.1525383!3d28.6921105!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8237f2fa865b70ac!2sEbslon%20Infotech-%20Website%20Designing%20And%20Website%20Development%20Company!5e0!3m2!1sen!2sin!4v1635147477102!5m2!1sen!2sin"
+                    width="100%" height="500" style="border:0;" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<?php include("footer.php");?> 
